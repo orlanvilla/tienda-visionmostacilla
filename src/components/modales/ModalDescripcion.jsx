59414bc65902d97a295335b779a864063ae78d1e
puntoexe77 <dixman.17@gmail.com>
2022-10-28 07:52:44 -0500

    Cmabio a modalesDescripcion en categorias/modales

--- src/components/modales/ModalDescripcion.jsx
+++ src/components/modales/ModalDescripcion.jsx
@@ -1,8 +1,10 @@
 import { useContext } from 'react'
 import { AppContext } from '../../context/AppContext'
 import './ModalDescripcion.css'
+import '../../admin/modales/ModalVistaProducto.css'
 import btn_close from '../../img/close.svg'
-import colibri_multicolor from '../../img/colibri-multicolor.jpeg'
+import icon_plus from '../../img/plus-circle.svg'
+import icon_dash from '../../img/dash-circle.svg'
 
 const ModalDescripcion = () => {
   const {setModal2,producto}=useContext(AppContext);
@@ -13,9 +15,9 @@ const ModalDescripcion = () => {
   }
 
   return (
-    <div className="contenedor-descripcion">
+    <div className="contenedor-productoss">
         
-        <div className="contenido-descripcion">
+        <div className="contenido-productoss">
           <img
             className='btn-close'
             alt='img_close'          
@@ -24,27 +26,32 @@ const ModalDescripcion = () => {
           />
           
           <div className='detalles'>
-            <section className='imagen-descripcion'>
+            <section className='imagen-productoss'>
               <img
-                className='img-descripcion'
+                className='img-productoss'
                 alt='img_descripcion'
                 src={producto.imagen}
-                style={{'width':'400px','height':'500px'}}
+                style={{'width':'30rem','height':'33rem'}}
               />
             </section>
             <section className='caracteristicas-producto'>
-            <h1>{producto.nombre}</h1>
-            <p>Categoría: <span>Colibrí llavero</span></p>
-            <p>Detalle del Producto</p>
-              <p>
+              <h1 className='h-1'>{producto.nombre}</h1>
+              <p className='p-p'>Categoría: <span>Colibrí llavero</span></p>
+              <p className='p-p'>Detalle del Producto</p>
+              <p className='p-p'>
                  {producto.descripcion}
               </p>
               <p>Cantidad: </p>
-              <input type="number" defaultValue={1}/>
-            
-              <p>Precio unitario: <span>{producto.precio}</span></p>
-            
-
+              <div className='botones-cantidad'>
+                <button>
+                    <img src={icon_dash} alt="logo restar" />
+                </button>
+                <input type="text" defaultValue={1} value='2' disabled/>
+                <button>
+                    <img src={icon_plus} alt="logo sumar" />
+                </button>
+              </div>
+              <p>Precio unitario: <span>{producto.precio}</span></p>            
               <input
                 type="submit"
                 value="Agregar al carrito"
